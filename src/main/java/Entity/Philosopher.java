390d1fce5f4a3353ad9d90d1234ed9441ebf403f
categorialNovum <beaumontjs@gmail.com>
2025-06-03 12:12:07 -0400

    working implementation. break deadlock by having all philosophers grab the left fork first except for the final philosopher, which will grab the right first.

--- src/main/java/Entity/Philosopher.java
+++ src/main/java/Entity/Philosopher.java
@@ -1,17 +1,70 @@
 package Entity;
 
+import com.sun.tools.javac.Main;
+
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 public class Philosopher implements Runnable{
     Random random = new Random();
+    Semaphore leftSemFork;
+    Semaphore rightSemFork;
+    int maxPhilosophers;
+
+    // 3 seconds eat time
+    private final int EAT_TIME = 3000;
+
+    public Philosopher(Semaphore lFork, Semaphore rFork){
+        this.leftSemFork = lFork;
+        this.rightSemFork = rFork;
+        this.maxPhilosophers = 5;
+    }
+    public Philosopher(Semaphore lFork, Semaphore rFork, int numPhilosophers){
+        this.leftSemFork = lFork;
+        this.rightSemFork = rFork;
+        this.maxPhilosophers = numPhilosophers;
+    }
 
     public void run(){
         try {
             while (true){
-                int waitTime = random.nextInt(1000);
-                System.out.println("Philosopher " + Thread.currentThread().getName() + " thinking for " + waitTime);
-                Thread.currentThread().wait(waitTime);
+                /** THINKING **/
+                int thinkTime = random.nextInt(1000);
+                System.out.println("Philosopher " + Thread.currentThread().getName() + " thinking for " + thinkTime);
+                Thread.sleep(thinkTime);
                 System.out.println("Philosopher " + Thread.currentThread().getName() + " done thinking");
+                /** ATTEMPT TO GRAB FORKS
+                 * Ensure one philosopher tries to pick up right before left to avoid deadlock **/
+
+                if (Integer.parseInt(Thread.currentThread().getName()) != maxPhilosophers) {
+                    synchronized (leftSemFork) {
+                        leftSemFork.acquire();
+                        System.out.println("Philosopher " + Thread.currentThread().getName() + " picked up left fork");
+                        synchronized (rightSemFork) {
+                            rightSemFork.acquire();
+                            System.out.println("Philosopher " + Thread.currentThread().getName() + " picked up right fork");
+                        }
+                    }
+                }else{
+                    synchronized (rightSemFork) {
+                        rightSemFork.acquire();
+                        System.out.println("Philosopher " + Thread.currentThread().getName() + " picked up right fork");
+                        synchronized (leftSemFork) {
+                            leftSemFork.acquire();
+                            System.out.println("Philosopher " + Thread.currentThread().getName() + " picked up left fork");
+                        }
+                    }
+
+                }
+                /** EAT **/
+                System.out.println("Philosopher " + Thread.currentThread().getName() + " eating");
+                Thread.sleep(EAT_TIME);
+                System.out.println("Philosopher " + Thread.currentThread().getName() + " done eating");
+                /** RETURN FORKS **/
+                leftSemFork.release();
+                System.out.println("Philosopher " + Thread.currentThread().getName() + " returned left fork");
+                rightSemFork.release();
+                System.out.println("Philosopher " + Thread.currentThread().getName() + " returned right fork");
             }
 
         }catch (Exception e){
@@ -19,7 +72,6 @@ public class Philosopher implements Runnable{
             System.out.println(e.getMessage());
             System.out.println("Philosopher interrupted : " + System.currentTimeMillis() + " : " + Thread.currentThread().getName());
             Thread.currentThread().interrupt();
-            return;
         }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,26 @@ package org.example;
 
 import Entity.Philosopher;
 
+import java.util.concurrent.Semaphore;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Initializing Dining Philosophers");
-        Philosopher[] diningPhilosophers = new Philosopher[5];
-
-        for (int i =0; i < diningPhilosophers.length; i++){
-            diningPhilosophers[i] = new Philosopher();
-            Thread thread = new Thread(diningPhilosophers[i], "PHILOSOPHER " + (i+1));
-            thread.start();
+        final int MAX_PHIL = 5;
+        System.out.println("Initializing Dining Philosophers with " + MAX_PHIL + " diners");
+        Philosopher[] diningPhilosophers = new Philosopher[MAX_PHIL];
+        Semaphore[] forks = new Semaphore[MAX_PHIL];
+        for (int x = 0; x < forks.length; x++){
+            /** Semaphore(MAX_AVAILABLE, FAIR?) **/
+            forks[x] = new Semaphore(1, true);
         }
 
+        for (int i =0; i < MAX_PHIL; i++){
+            Semaphore lFork = forks[i];
+            Semaphore rFork = forks[(i +1) % forks.length];
+            diningPhilosophers[i] = new Philosopher(lFork, rFork, MAX_PHIL);
+            //Thread thread = new Thread(diningPhilosophers[i], "PHILOSOPHER " + (i+1));
+            Thread thread = new Thread(diningPhilosophers[i], Integer.toString((i+1)));
+            thread.start();
+        }
     }
 }
